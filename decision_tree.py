fd50791e78c904ffb8e9813c38a10bf001493e34
Daryl Fung <darylfung96@gmail.com>
2017-12-18 00:23:55 -0600

    added fit function

--- decision_tree.py
+++ decision_tree.py
@@ -1,26 +1,49 @@
 import numpy as np
+import random
+
+from Information_helper import information_gain
 
 class DecisionTree:
     def __init__(self, max_depth=10, minimum_sample_split=2):
         self.max_depth = max_depth
         self.minimum_sample_split = minimum_sample_split
 
-    def fit(self, X, y):
-        pass
+    def fit(self, X, y, is_random_forest=True):
+        num_features = X.shape[1]
 
-    def _build_tree(self):
+        if is_random_forest:
+            num_features = X.shape[1]
+            total_feature_to_extract = np.sqrt(num_features)
+            feature_indexes = random.sample(range(num_features), total_feature_to_extract)
+        else:
+            feature_indexes = num_features
+
+        self._build_tree(X, y, feature_indexes)
+
+    # do recursion in build tree function
+    def _build_tree(self, X, y, feature_indexes):
         pass
 
     def _find_best_split(self, X, y, feature_indexes):
+
+        best_gain = 0
+        best_feature_index = 0
+        best_threshold = 0
 
         for feature_index in feature_indexes:
             current_x = sorted(set(X[:, feature_index]))
 
             for iteration in range(len(current_x)-1):
                 threshold = (current_x[iteration]+current_x[iteration+1])/2  # for continuous variable
-                self._split(X, y, feature_index, threshold)
+                x_left, y_left, x_right, y_right = self._split(X, y, feature_index, threshold)
+                gain = information_gain(y, y_left, y_right)
 
-        pass
+                if gain > best_gain:
+                    best_gain = gain
+                    best_feature_index = feature_index
+                    best_threshold = threshold
+
+        return best_feature_index, best_threshold
 
     def _split(self, X, y, feature_index, threshold):
         X_left = []
